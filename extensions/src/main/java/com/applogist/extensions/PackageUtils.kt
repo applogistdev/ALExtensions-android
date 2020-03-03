75f510c3b7a0d278af53b02925c45eaa2b658660
Mustafa Urgupluoglu <mustafa.urgupluoglu@applogist.com>
2020-03-03 14:20:10 +0300

    gradle updated -> 3.6.1, openGooglePlayStore added

--- extensions/src/main/java/com/applogist/extensions/PackageUtils.kt
+++ extensions/src/main/java/com/applogist/extensions/PackageUtils.kt
@@ -1,7 +1,10 @@
 package com.applogist.extensions
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.util.Log
 
 /*
@@ -9,10 +12,26 @@ import android.util.Log
 *  Copyright © 2020 Mustafa Ürgüplüoğlu. All rights reserved.
 */
 
-fun Context.isAppInstalled(packageName : String): Boolean = try {
+fun Context.isAppInstalled(packageName: String): Boolean = try {
     packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES)
     true
 } catch (e: PackageManager.NameNotFoundException) {
     Log.e("PackageUtils", e.toString())
     false
 }
+
+fun Context.openGooglePlayStore(packageName: String, tryWithWebView: Boolean = true): Boolean =
+    try {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")))
+        true
+    } catch (e: ActivityNotFoundException) {
+        if (tryWithWebView) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+                )
+            )
+        }
+        false
+    }
