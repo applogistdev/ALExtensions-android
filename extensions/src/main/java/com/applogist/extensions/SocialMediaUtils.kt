6e4129a9b22ce51f79f4be3696b517d7fe59ec7d
murgupluoglu <masadov@gmail.com>
2020-03-25 17:43:17 +0300

    openFacebookPage fixed

--- extensions/src/main/java/com/applogist/extensions/SocialMediaUtils.kt
+++ extensions/src/main/java/com/applogist/extensions/SocialMediaUtils.kt
@@ -2,6 +2,7 @@ package com.applogist.extensions
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.util.Log
 
@@ -14,14 +15,27 @@ import android.util.Log
  * @param pageId Facebook page id
  * @return true if Facebook app found
  */
-fun Context.openFacebookPage(pageId : String): Boolean = try {
-    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/${pageId}"))
-    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-    startActivity(intent)
-    true
-}catch (e : Exception){
-    Log.e("SocialMediaUtils", e.toString())
-    false
+fun Context.openFacebookPage(facebookPageID : String): Boolean {
+    val facebookUrl = "https://www.facebook.com/$facebookPageID"
+    val facebookUrlScheme = "fb://page/$facebookPageID"
+    try {
+        val versionCode = packageManager.getPackageInfo("com.facebook.katana", 0).versionCode
+        if (versionCode >= 3002850) {
+            val uri = Uri.parse("fb://facewebmodal/f?href=$facebookUrl")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        } else {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrlScheme))
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+        return true
+    } catch (e: PackageManager.NameNotFoundException) {
+        Log.e("SocialMediaUtils", e.toString())
+    }
+
+    return false
 }
 
 /**
